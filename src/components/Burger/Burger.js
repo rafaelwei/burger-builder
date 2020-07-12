6c179998e61b46a2dcb881ae42e49b4dd2bcd21c
rafaelwei <rafaelwei07@gmail.com>
2020-07-12 17:49:48 -0300

    Fixed 'Aux' file name problem

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import classes from './Burger.css';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+
+const Burger = (props) => {
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(ingredientKey => {
+            return [...Array(props.ingredients[ingredientKey])].map((_, i) => {
+                return <BurgerIngredient key = {ingredientKey + i} type={ingredientKey}/>
+            });
+        })
+    return(
+        <div className={classes.Burger}>
+            <BurgerIngredient type="bread-top"/>
+            {transformedIngredients}
+            <BurgerIngredient type="bread-bottom"/>
+        </div>
+    );
+};
+
+export default Burger;
