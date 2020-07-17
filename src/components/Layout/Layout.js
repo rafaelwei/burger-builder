3fb33f5bd56ea465982923eaf100884abd34796b
rafaelwei <rafaelwei07@gmail.com>
2020-07-16 23:48:56 -0300

    Added toolbar and logo to the application

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,14 +2,15 @@ import React from 'react';
 
 import Aux from '../../hoc/Auxiliary';
 import classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Tollbar';
 
 const Layout = (props) => {
     return(
         <Aux>
-            <div>dfjskafasdjkfbakjdsf</div>
-                <main className={classes.Content}>
-                    {props.children}
-                </main>
+            <Toolbar/>
+            <main className={classes.Content}>
+                {props.children}
+            </main>
         </Aux>
     )
 };
